cd3c581af065c2e68d144181c80d89e74d63f6da
NittlestX <6434444823@student.chula.ac.th>
2025-04-19 20:38:31 +0700

    add index2.js

--- index2.js
+++ index2.js
@@ -93,6 +93,10 @@ app.get('/', (req, res) => {
 });
 
 // Start server
+//app.listen(PORT, () => {
+//  console.log(`Server is running at http://localhost:${PORT}`);
+//});
+
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
