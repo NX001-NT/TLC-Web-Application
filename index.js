6aeef178039e5950df5e726b821bf4d628e49fbe
NittlestX <6434444823@student.chula.ac.th>
2025-04-19 15:30:18 +0700

    add index.js

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const uploadDir = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, 'uploads/');
